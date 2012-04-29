182dbdbd3355ca6d6295f920804ffe445f35431e
Voytenko Yulia <uwhy10@gmail.com>
2012-04-29 19:38:08 +1200

    Update Code/play-2.0/gp/app/models/DataExtraction.java

--- Code/play-2.0/gp/app/models/DataExtraction.java
+++ Code/play-2.0/gp/app/models/DataExtraction.java
@@ -4,15 +4,12 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.avaje.ebean.Ebean;
-//import java.util.Locale;
-
 public class DataExtraction {
 
     public int newGPM(String id) {
 	GPM gpm = new GPM(id);
 	GPM.create(gpm); 
-	updateActivity(gpm,100);
+	//updateActivity(gpm,100);
         // i should add a validator!
     updateProfile(gpm);
     return 0;
@@ -31,8 +28,8 @@ public class DataExtraction {
 		} // i should add a validator!
 
         //add profile to DB
-    Gender gender =  Gender.find.where().eq("value", profile.gender).findUnique();
-    Relationship relationshipStatus = Relationship.find.where().eq("status", profile.relationshipStatus).findUnique();
+    Gender gender =  Gender.findByKind(profile.gender);
+    Relationship relationshipStatus = Relationship.findByKind(profile.relationshipStatus);
 	Profile man = new Profile(gpm,profile.displayName,profile.image, gender,
 					profile.tagline, relationshipStatus,100);
 	Profile.create(man);
@@ -47,13 +44,15 @@ public class DataExtraction {
         for (HistogramForWords pH: profileHistogram) {
                 //for pH.word search it (word) in WordDictionary
                 Word wordFromDictionary = Word.findByWord(pH.word);
-                Synonym wordFromSynonyms = Synonym.find.where().eq("synonym", pH.word).findUnique();
+                List <Synonym> word2 = Synonym.findBySynonym(pH.word);
                 //there is the word (pH.word) in dictionary (Word or Synonym)
-                if ((wordFromDictionary != null) || (wordFromSynonyms != null)) {
+                if ((wordFromDictionary != null) || !(word2.isEmpty())) {
                     //add word from profile to table 'ProfileWord'
                     if (wordFromDictionary == null) {
+					    Synonym wordFromSynonyms = new Synonym();
+                        wordFromSynonyms = word2.get(0);
                     	Word word = wordFromSynonyms.word;			
-			ProfileWord profileWord = new ProfileWord(man, word, pH.countWord);	
+			            ProfileWord profileWord = new ProfileWord(man, word, pH.countWord);	
                         ProfileWord.create(profileWord);
                     } else {
                     	ProfileWord profileWord = new ProfileWord(man, wordFromDictionary, pH.countWord);
@@ -85,7 +84,8 @@ public class DataExtraction {
         //for each 'post' from 'activity'
         for (TempPost post: activity) {
             //add posts to DB (class Post)
-        	Content kindContent =  Content.find.where().eq("kind", post.kindContent).findUnique();
+        	post.print();
+        	Content kindContent =  Content.findByKind(post.kindContent);
             Post postToDB = new Post (gpm, post.postId, post.publishedData, kindContent, post.nComments,
 				post.nPlusOne, post.nResharers,post.isRepost);
             Post.create(postToDB);
@@ -99,13 +99,15 @@ public class DataExtraction {
             for (HistogramForWords pH: postHistogram) {
                 //for pH.word search it (word) in WordDictionary
             	Word wordFromDictionary = Word.findByWord(pH.word);
-                Synonym wordFromSynonyms = Synonym.find.where().eq("synonym", pH.word).findUnique();
+                List <Synonym> word2 = Synonym.findBySynonym(pH.word);
                 //there is the word (pH.word) in dictionary (WordDictionary or WordSynonyms)
-                if ((wordFromDictionary!=null) || (wordFromSynonyms!=null)) {
+                if ((wordFromDictionary!=null) || !(word2.isEmpty())) {
                     //add word from post to table 'PostWord'
                     if (wordFromDictionary==null) {
-                    	Word word = wordFromSynonyms.word;			
-		        PostWord postWord = new PostWord(postToDB, word, pH.countWord);	
+		        Synonym wordFromSynonyms = new Synonym();
+                        wordFromSynonyms = word2.get(0);
+                       	Word word = wordFromSynonyms.word;			
+			            PostWord postWord = new PostWord(postToDB, word, pH.countWord);	
                         PostWord.create(postWord);
                     } else {
                         PostWord postWord = new PostWord(postToDB, wordFromDictionary, pH.countWord);	
@@ -121,7 +123,7 @@ public class DataExtraction {
             if ((post.isRepost)) {
                 NewGPM newId =  NewGPM.find.where().eq("id_gpm", post.actorId).findUnique();
                 if (newId==null) {
-                    NewGPM newGpm= new NewGPM(post.actorId,1);
+					NewGPM newGpm= new NewGPM(post.actorId,1);
                     NewGPM.create(newGpm);
                     // i should add a validator!
                 } else {
@@ -146,7 +148,7 @@ public class DataExtraction {
         }
         HistogramForWords elem = new HistogramForWords();
         elem.countWord++;
-        elem.word = mas.get(0);//на этом месте ломается
+        elem.word = mas.get(0);
         hist.add(elem);
         mas.remove(0);
         for (String m: mas){
