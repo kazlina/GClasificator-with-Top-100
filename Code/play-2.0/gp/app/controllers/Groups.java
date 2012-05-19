89c03d092a1ea1decef78a609a8055024aabe625
JulyP <jpishchikova@gmail.com>
2012-05-19 23:33:55 +1100

    added page for groupwords

--- Code/play-2.0/gp/app/controllers/Groups.java
+++ Code/play-2.0/gp/app/controllers/Groups.java
@@ -20,6 +20,7 @@ import views.html.*;
 public class Groups extends Controller {
 
     static Form<Group> groupForm = form(Group.class);
+    static Form<GroupWord> groupWordForm = form(GroupWord.class);
 
     // GROUP
     public static Result groups() {
@@ -39,5 +40,25 @@ public class Groups extends Controller {
     public static Result deleteGroup(Long id) {
         Group.delete(id);
         return redirect(routes.Groups.groups());
+    }
+
+    // GROUP WORDS
+        public static Result groupWords(Long groupId) {
+        return ok(views.html.groupword.render(Group.findById(groupId), Group.findById(groupId).name, GroupWord.findByGroup(groupId), groupWordForm));
+    }
+
+    public static Result addGroupWord(Long groupId) {
+        Form<GroupWord> filledForm = groupWordForm.bindFromRequest();
+        if(filledForm.hasErrors()) {
+            return badRequest(views.html.groupword.render(Group.findById(groupId), Group.findById(groupId).name, GroupWord.findByGroup(groupId), filledForm));
+        } else {
+            GroupWord.add(filledForm.get());
+            return redirect(routes.Groups.groupWords(groupId));
+        }
+    }
+
+    public static Result deleteGroupWord(Long groupId,Long groupWordId) {
+        GroupWord.delete(groupWordId);
+        return redirect(routes.Groups.groupWords(groupId));
     }
 }
