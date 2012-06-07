651ebf746b0e9cefa3506711cf88203294d2305e
Artyom <necroart@myopera.com>
2012-06-07 13:03:33 +1100

    debug link extr in progress

--- Code/play-2.0/gp/test/modelsTests/LinkTest.java
+++ Code/play-2.0/gp/test/modelsTests/LinkTest.java
@@ -1,10 +1,20 @@
 package modelsTests;
-
+import org.junit.*;
+import java.util.*;
+import static play.test.Helpers.*;
+import static org.fest.assertions.Assertions.*;
 import models.*;
 
 public class LinkTest {
 	@Test
-	public void () {
-		
+	public void t() {
+		String testStringMass = "<a href = http://www.google.com>google</a>" +
+				"<a href =http://www.google.com >google</a>" +
+				"<a href = http://www.google.com >google</a>" +
+				"<a href = \"http://www.google.com\">google</a>" +
+				"<a href =http://www.google.com>google</a>" +
+				"<a href =\"http://www.google.com\">google</a>" +
+				"<a href=\"http://www.google.com\">google</a>";
+		//linkPreprocessor (testStringMass, 3);
 	}
 }
