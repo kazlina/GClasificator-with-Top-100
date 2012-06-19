4aac00db83a1d0efaafb8512d70a6dbf9d4619b4
Yuri Vashchenko <yuri.vashchenko@gmail.com>
2012-06-20 10:59:26 +1100

    Modify update controller

--- Code/play-2.0/gp/app/controllers/Application.java
+++ Code/play-2.0/gp/app/controllers/Application.java
@@ -1,43 +1,40 @@
 package controllers;
 import java.util.*;
 import java.lang.InterruptedException;
+import java.util.concurrent.TimeUnit;
 import play.mvc.*;
 import models.*;
 import java.io.IOException;
 
 public class Application extends Controller {
 
-	private static class BackgroundProcess implements Runnable {
-		public void run() {
-			while (true) {
-				try {
-					UpdateControl.Start();
-		    	}
-		    	catch (InterruptedException ex) {}
-			}
-		}
-	}
-	
-	private static boolean threadRun;
-	
-	public static Result ind() throws IOException {
-		TempProfile temp = GAPI.getProfile("101793532287583914396");
-	    return ok(temp.displayName);
-	}
-
+    private static class BackgroundProcess implements Runnable {
+        public void run() {
+            while (true) {
+                try {
+                    UpdateControl.Start();
+                    TimeUnit.SECONDS.sleep(10);
+                }
+                catch (InterruptedException ex) {}
+            }
+        }
+    }
+    
+    private static boolean threadRun;
+    
     public static Result index() {
-    	if (!threadRun) {
-    		Thread worker = new Thread(new BackgroundProcess());
-    		worker.setDaemon(true);
-    		threadRun = true;
-    		worker.start();
-    	}
-    	
-    	return ok(views.html.index.render(Group.all()));
+        if (!threadRun) {
+            Thread worker = new Thread(new BackgroundProcess());
+            worker.setDaemon(true);
+            threadRun = true;
+            worker.start();
+        }
+        
+        return ok(views.html.index.render(Group.all()));
     }
     
     public static Result viewGroup(Long idGroup) {
-    	List <Profile> gpms = Classifier.getGpmForGroup(idGroup);
-    	return ok(views.html.usergroup.render(gpms, Group.all(), Group.findById(idGroup).name));
+        List <Profile> gpms = Classifier.getGpmForGroup(idGroup);
+        return ok(views.html.usergroup.render(gpms, Group.all(), Group.findById(idGroup).name));
     }
 }
