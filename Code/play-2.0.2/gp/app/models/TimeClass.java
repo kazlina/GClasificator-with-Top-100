a197f1395322b5e3a1cf56df6d2a8f94107d3cc9
Artyom <necroart@myopera.com>
2012-11-01 04:18:19 +1100

    add function for get difference between 2 dates

--- Code/play-2.0.2/gp/app/models/TimeClass.java
+++ Code/play-2.0.2/gp/app/models/TimeClass.java
@@ -1,5 +1,7 @@
 package models;
+import java.sql.Time;
 import java.util.Date;
+import java.util.Calendar;
 
 public class TimeClass {
 	public static void printCurrentTime () {
@@ -14,5 +16,14 @@ public class TimeClass {
 		System.out.print("Current time is ");
 		TimeClass.printlnCurrentTime();
 		System.out.println("_____________________________________________");
+	}
+	public static Calendar getDuration (Calendar firstCalendar, Calendar secondCalendar) {
+		int hours, minutes, seconds; 
+		hours = secondCalendar.get(Calendar.HOUR_OF_DAY) - firstCalendar.get(Calendar.HOUR_OF_DAY);
+		minutes = secondCalendar.get(Calendar.MINUTE) - firstCalendar.get(Calendar.MINUTE);
+		seconds = secondCalendar.get(Calendar.SECOND) - firstCalendar.get(Calendar.SECOND);
+		Calendar newCalendar = Calendar.getInstance();
+		newCalendar.set(0, 0, 0, hours, minutes, seconds); //newCalendar.get(Calendar.HOUR_OF_DAY) newCalendar.get(Calendar.MINUTE) newCalendar.get(Calendar.SECOND)
+		return newCalendar;
 	}
 }
